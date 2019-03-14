9c9ae6f3d5516dfb51e6d1d46b1f650d3e886c68
BT_1N3_13 <sfaruk3131@bths.edu>
2019-03-14 10:07:18 -0400

    dgjhf

--- src/People/Player.java
+++ src/People/Player.java
@@ -1,6 +1,35 @@
 package People;
 
 public class Player {
-    private int hP;
-    private int sttk;
+    public int hP;
+    public int sttk;
+    public int xLoc, yLoc;
+
+    public int getxLoc()
+    {
+        return xLoc;
+    }
+
+    public void setxLoc(int xLoc)
+    {
+        this.xLoc = xLoc;
+    }
+
+    public int getyLoc()
+    {
+        return yLoc;
+    }
+
+    public void setyLoc(int yLoc)
+    {
+        this.yLoc = yLoc;
+    }
+
+    public void Person (String firstName,int xLoc, int yLoc)
+    {
+        this.xLoc = xLoc;
+        this.yLoc = yLoc;
+    }
+
+
 }
